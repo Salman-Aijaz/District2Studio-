0282819b3cdcc4d5547519094fa4174317b438f2
Salman-Aijaz <salman333699@gmail.com>
2024-09-17 08:12:21 -0700

    hero section image work

--- src/Pages/HeroSection.tsx
+++ src/Pages/HeroSection.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { DownIcon, RightArrowIcon } from "../assets/Icon/Icon";
 import "../App.css"
+import CoverBob from "../assets/Cover_Bob.jpg"
 
 const HeroSection = () => {
   const [activeIndex, setActiveIndex] = useState(null);
@@ -8,16 +9,7 @@ const HeroSection = () => {
 
   const handleClick = (index) => {
     setActiveIndex(index);
-  };
-    // const h1Ref = useRef(null);
-    // const spanRef = useRef(null);
-  
-    // useEffect(() => {
-    //   if (h1Ref.current && spanRef.current) {
-    //     const h1Width = h1Ref.current.offsetWidth;
-    //     spanRef.current.style.width = `${h1Width}px`;
-    //   }
-    // }, []);
+  }; 
 
   return (
     <div>
@@ -28,39 +20,42 @@ const HeroSection = () => {
             concept to creation, we transform ideas into impactful digital
             experiences
           </h1>
-          <div className="text-white pr-3 relative group">
-            <p>Industry:</p>
-            <span className="font-bold">Agency</span>
-            <div className="min-h-[40px]"></div>
-            <h1 className='text-black text-[4rem] tracking-[-1px]'>
-            Est Populo
-            </h1>
-            <span
-              className={`absolute text-xs bottom-[-24px] left-0 h-[1px] bg-black transition-all
-                    w-0 group-hover:w-full
-                    duration-300 ease-out`}
-            ></span>
-          </div>
         </div>
-
-        <div className="flex gap-3 self-center items-center pl-3">
+        <div className="flex gap-3 self-end items-center pl-3 pt-[3.5rem]">
           <p>We bridge the raw to beautiful</p>
           <div className="cursor-pointer ">
             <RightArrowIcon />
           </div>
         </div>
       </div>
+      <div className="text-white lg:pr-3 relative group w-3/4 xl:w-[90%] px-3 mx-auto">
+            <p>Industry:</p>
+            <span className="font-bold">Agency</span>
+            <div className="min-h-[40px]"></div>
+            <h1 className='text-black text-[4rem] tracking-[-1px] min-w-fit'>
+            Est Populo
+            </h1>
+            <span
+              className={`absolute text-xs bottom-[-24px] left-0 h-[1px] bg-black transition-all
+                    w-0 group-hover:max-w-fit
+                    duration-300 ease-out`}
+            ></span>
+          </div>
 
-      <div className="flex justify-between pt-12 pl-10">
+      <div className="">
         <span className="cursor-pointer">
        <DownIcon/>
         </span>
+        
+      <div className="h-[49.47917vh]">
+          <img src={CoverBob} alt="" className="object-cover w-full h-full object-center mb-48" />
+        </div>
         <div className="h-[48.61111vh] absolute bottom-0 right-0 w-[6.66667rem] z-30">
          <ul className="absolute top-1/2 -translate-y-1/2 right-[3.33333rem] list-none cursor-pointer">
          <li className="w-[0.88889rem] h-[0.88889rem] mb-[0.88889rem] relative custom-li" onClick={() => handleClick(0)} ></li>         
-         <li className="w-[0.88889rem] h-[0.88889rem] mb-[0.88889rem] relative" onClick={() => handleClick(1)}></li>
-         <li className="w-[0.88889rem] h-[0.88889rem] mb-[0.88889rem] relative" onClick={() => handleClick(2)}></li>
-         <li className="w-[0.88889rem] h-[0.88889rem] mb-[0.88889rem] relative" onClick={() => handleClick(3)}></li>
+         <li className="w-[0.88889rem] h-[0.88889rem] mb-[0.88889rem] relative custom-li" onClick={() => handleClick(1)}></li>
+         <li className="w-[0.88889rem] h-[0.88889rem] mb-[0.88889rem] relative custom-li" onClick={() => handleClick(2)}></li>
+         <li className="w-[0.88889rem] h-[0.88889rem] mb-[0.88889rem] relative custom-li" onClick={() => handleClick(3)}></li>
          </ul>
         </div>
       </div>
