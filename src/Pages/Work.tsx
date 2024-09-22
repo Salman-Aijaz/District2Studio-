bc99cc79912c366069dd535d5d11949a0cd661cf
Salman-Aijaz <salman333699@gmail.com>
2024-09-22 08:21:39 -0700

    transiton

--- src/Pages/Work.tsx
+++ src/Pages/Work.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import coverBob from "../assets/images/Cover_Bob.jpg"
 import EstPopulo from "../assets/images/Populo.jpg"
 import SomethingGood from "../assets/images/something good.jpg"
@@ -8,6 +8,9 @@ import Thea from "../assets/images/thea.jpg"
 import DinoGroup from "../assets/images/dinogroup.jpg"
 import CryptoSwap from "../assets/images/cryptoswap.jpg"
 import "../App.css";
+import AOS from "aos"
+import 'aos/dist/aos.css';
+
 
 const works = [
   { project: "Bobbob", industry: "fintech", services: "app", image: coverBob },
@@ -21,23 +24,32 @@ const works = [
 ]
 
 const Work = () => {
+
+  useEffect(()=>{
+  AOS.init({ duration: 1000 , easing: 'ease-in-out', once: false })
+  },[])
+
   return (
     <div className='container mx-auto px-3'>
       <div className='min-h-36'></div>
-      <h1 className='font-light tracking-tight text-4xl sm:text-4xl md:text-6xl lg:text-7xl leading-5 mb-20'>Our Work</h1>
+      <h1 className='font-light tracking-tight text-4xl 
+      sm:text-4xl md:text-6xl lg:text-7xl leading-5 mb-20' data-aos="fade-up">
+        Our Work</h1>
       <div className='grid gap-10 grid-cols-1 md:grid-cols-2'>
         {works.map((work, index) => {
           return (
             <div key={index} className='overflow-hidden relative group'>
               <a href="">
-                <div className='mb-2'>
+                <div className='mb-2' data-aos="fade-up">
                   <div className='h-[400px] xl:h-[42.66667rem]  overflow-hidden relative'>
-                    <h1 className='text-center text-white text-6xl absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10'>
+                    <h1 className='text-center text-white text-6xl absolute 
+                    inset-0 flex justify-center items-center opacity-0 
+                    group-hover:opacity-100 transition-opacity duration-300 z-10' 
+                    >
                       View Project
                     </h1>
                     <img src={work.image} width="2560" height="1440" alt="" 
-                    className='object-cover h-full w-full group-hover:scale-125 transition-transform 
-                    duration-700' />
+                    className='object-cover h-full w-full group-hover:scale-110 duration-700 transition-transform' />
                   </div>
                 </div>
                 <div className='flex flex-wrap'>
